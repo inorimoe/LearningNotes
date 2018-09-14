3131d024ac95ac11067b393e8014175ee8c81875
inorimoe <finory@outlook.com>
2018-09-14 21:31:17 +0800

    easy

---
+++ LeetCode/58. Length of Last Word.c
@@ -0,0 +1,21 @@
+int lengthOfLastWord(char* s) {
+    if(!*s)return 0;
+    char* worLeft,*worRight;
+    worLeft=worRight=s;
+    //用单词的最右边减去单词的最左边+1
+    while(*s)
+    {
+        if(*s!=' '&&*(s-1)==' ')
+        {
+            worLeft = s;
+        }
+        if(*s!=' '&&(*(s+1)==' '||*(s+1)=='\0'))
+        {
+            worRight = s;
+        }
+        ++s;
+    }
+    if(*worLeft==' ')return 0;
+    //字符串如果为零空白，worleft与worright会在字符串最左端不动
+    return worRight-worLeft+1;
+}
