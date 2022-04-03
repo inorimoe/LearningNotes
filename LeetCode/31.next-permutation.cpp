5521d1658a9265d65d92f1c309d7c9019b8137d9
Finory <finory@outlook.com>
2022-04-03 18:47:45 +0800

    medium

--- LeetCode/31.next-permutation.cpp
+++ LeetCode/31.next-permutation.cpp
@@ -1,10 +1,3 @@
-/*
- * @lc app=leetcode id=31 lang=cpp
- *
- * [31] Next Permutation
- */
-
-// @lc code=start
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
@@ -20,5 +13,19 @@ public:
         sort(nums.begin() + idx, nums.end());
     }
 };
-// @lc code=end
+//最优解用reverse，画图只想到排序，没想到这个思路
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int idx = nums.size() - 1;
+        for(;idx-1 > -1 && nums[idx-1] >= nums[idx]; --idx){};
+        reverse(nums.begin() + idx, nums.end());
+        if(idx==0){return;}
+        int jdx = idx;
+        for(; nums[jdx] <= nums[idx-1] ; ++jdx){};
+        swap(nums[idx-1], nums[jdx]);
+    }
+};
+
+
 
